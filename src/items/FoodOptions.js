01c944b8ab76896a70aea067e59c94e64062caa0
djarreau <djgco360@gmail.com>
2022-07-22 18:12:19 -0400

    first deploy

--- src/items/FoodOptions.js
+++ src/items/FoodOptions.js
@@ -9,5 +9,5 @@ export const FoodMenuList = [
     { id: 8, title: 'Chicken Wings', option1: 'Platter - $16', option2: '5 wings - $8', price: '16', src: '/assets/images/chicken_wings.jpg' },
     { id: 9, title: 'Fried Shrimp', option1: 'Platter - $20', option2: 'Dozen - $14', price: '20', src: '/assets/images/fried_shrimp.jpg' },
     { id: 10, title: 'Famous Fruit Drinks', option1: '1 Bottle - $3', option2: '', price: '3', src: '/assets/images/flavored_drinks.jpg' },
-    { id: 11, title: 'Homemade Desserts', option1: 'Rice Pudding - $6', option2: 'Banana Pudding - $6', price: '6', src: './assets/images/rice_pudding.jpg' },
+    { id: 11, title: 'Homemade Desserts', option1: 'Rice Pudding - $6', option2: 'Banana Pudding - $6', price: '6', src: '/assets/images/rice_pudding.jpg' },
 ]
